0dbeef38cfee33975aef012ef38ed69a28f00125
GandyCSitumorang <43582547+GandyCSitumorang@users.noreply.github.com>
2018-10-02 11:41:48 +0700

    Update PBO610116228Latihan17ProgramTunjangan.java

--- src/pbo6/pkg10116228/latihan17/programtunjangan/PBO610116228Latihan17ProgramTunjangan.java
+++ src/pbo6/pkg10116228/latihan17/programtunjangan/PBO610116228Latihan17ProgramTunjangan.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
  * @author 
  * NAMA : Gandy Christian Situmorang
  * KELAS: PBO-6
- * NIM  : 10116338
+ * NIM  : 10116228
  * Deskripsi program: program ini berisi program untuk menampilkan hasil 
  * perhitungan gaji berserta tunjangannya
  */
